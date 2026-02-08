048ee1d690b49ea597f9f38f1562214fdabfd1bd
m1v8a <245783116+m1v8a@users.noreply.github.com>
2026-02-08 17:07:23 +0800

    feat: add style for the task form

--- src/components/task-section/taskFormComp.js
+++ src/components/task-section/taskFormComp.js
@@ -1,30 +1,46 @@
+const randomTitles = [
+  "Learn Javascript",
+  "Build a website",
+  "Take the dog to the vet",
+];
+
 export default function taskFormComp() {
   const container = document.createElement("form");
   container.noValidate = true;
   container.action = "#";
+  container.id = "task-section";
+
+  const randomTitle = randomTitles[Math.floor(Math.random() * 3)];
+
   container.innerHTML = `
-  <div>
-    <input type="text" name="task-title-input">
-    <textarea name="task-note-input"></textarea>
-    <input type="date" name="task-due-input">
+  <div class="main-inputs">
+    <input 
+        type="text"
+        name="task-title-input" 
+        class="task-title-input" 
+        placeholder="${randomTitle}">
+    <textarea name="task-note-input" class="task-note-input" placeholder="Note..."></textarea>
+    <input type="date" name="task-due-input" class="task-due-input">
   </div>
-  <div>
-    <label>
-      <input type="radio" value="1" name="task-priority-input">
-      Urgent
-    </label>
-    <label>
-      <input type="radio" value="2" name="task-priority-input">
-      Should do
-    </label>
-    <label>
-      <input type="radio" value="3"name="task-priority-input">
-      Optional
-    </label>
+  <div class="second-inputs">
+    <ul class="task-priority-input">
+      <label>
+        <input type="radio" value="1" name="task-priority-input">
+        Urgent
+      </label>
+      <label>
+        <input type="radio" value="2" name="task-priority-input">
+        Should do
+      </label>
+      <label>
+        <input type="radio" value="3"name="task-priority-input">
+        Optional
+      </label>
+    </ul>
   </div>
-  <div>
+  <div class="task-buttons">
     <button data-name="add-task-submit">Add</button>
-    <button class="bg-danger" data-name="close-task-form">X</button>
+    <!-- <button class="bg-danger" data-name="close-task-form">X</button> -->
   </div>
   `;
   return container;
--- src/components/task-section/taskSectionComp.js
+++ src/components/task-section/taskSectionComp.js
@@ -1,5 +1,6 @@
 import taskFormComp from "./taskFormComp.js";
 import taskListComp from "./taskListComp.js";
+import "./style.css";
 
 export default function taskSectionComp() {
   const container = document.createElement("section");
